8b92b5a1a0acbfc380ab2dd3970d6beef6628f5f
Menelik Tefera <menelikworku@gmail.com>
2022-02-08 12:34:59 +0100

    Updates

---
+++ precache-manifest.185840ce7d58261d1045a684e8754135.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d7d55c42a7757435308",
+    "url": "/seasons/static/css/main.0f01ae93.chunk.css"
+  },
+  {
+    "revision": "3d7d55c42a7757435308",
+    "url": "/seasons/static/js/main.17e403c4.chunk.js"
+  },
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  },
+  {
+    "revision": "59ce20cfe470e6497456",
+    "url": "/seasons/static/js/2.1f26c565.chunk.js"
+  },
+  {
+    "revision": "061892b550e9d745a5d2afac0d046666",
+    "url": "/seasons/index.html"
+  }
+];
